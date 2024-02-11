9625c595d95fe7de0e4af0c183801a7d53bb51de
Lufajs <faronludwik@gmail.com>
2024-02-11 19:28:59 +0100

    .

--- app/components/main/Navbar.tsx
+++ app/components/main/Navbar.tsx
@@ -7,11 +7,11 @@ import { HeartIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 export default function Navbar() {
   return (
-    <div className="absolute flex w-full left-[50%] translate-x-[-50%] justify-between text-lg pt-3 z-10 select-none">
+    <div className="absolute flex w-full left-[50%] translate-x-[-50%] justify-between items-center text-lg pt-3 z-10 select-none">
       <Link href={"/"} className="ml-20">
         <ul>
           <li className="text-xl flex items-center gap-1">
-            <div className="w-[30x] h-[30px]">
+            <div className="min-w-[30px] h-[30px]">
               <PostImage src={"/yvlogo.png"} />
             </div>
             <div
@@ -22,8 +22,8 @@ export default function Navbar() {
           </li>
         </ul>
       </Link>
-      <ul className="flex items-center text-[#222] ml-32">
-        <div className="w-[500px]">
+      <ul className="flex items-center w-[30%] text-[#222] ml-48 mr-5">
+        <div className="w-[100%]">
           <form>
             <div className="rounded-lg flex">
               <input
@@ -44,22 +44,31 @@ export default function Navbar() {
         </div>
       </ul>
       <ul className="flex items-center justify-center gap-6">
-        <Link href={"/yerba"} className="cursor-pointer hover:text-pageTheme">
-          Yerbas
+        <Link
+          href={"/yerba"}
+          className="cursor-pointer hover:text-pageTheme min-w-[92px]"
+        >
+          Yerba Mate
         </Link>
-        <Link href={""} className="cursor-pointer hover:text-pageTheme">
-          Suggest
+        <Link
+          href={"/top"}
+          className="cursor-pointer hover:text-pageTheme min-w-[82px]"
+        >
+          Top Rated
         </Link>
         <Link href={""} className="cursor-pointer hover:text-pageTheme">
           About
         </Link>
-        <Link href={""} className="cursor-pointer hover:text-pageTheme">
+        <Link
+          href={"/favorite"}
+          className="cursor-pointer hover:text-pageTheme"
+        >
           <HeartIcon className="w-7" />
         </Link>
         <span className="text-[#888]">|</span>
         <Link
           href={"/login"}
-          className="cursor-pointer hover:text-pageTheme mr-5"
+          className="cursor-pointer hover:text-pageTheme mr-5 min-w-[55px]"
         >
           Sign In
         </Link>
