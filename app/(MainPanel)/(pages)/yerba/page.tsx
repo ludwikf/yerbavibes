b77bbcd62d70297efdf8756ab672a98166e0b59e
Lufajs <faronludwik@gmail.com>
2024-02-23 16:16:01 +0100

    .

--- app/(MainPanel)/(pages)/yerba/page.tsx
+++ app/(MainPanel)/(pages)/yerba/page.tsx
@@ -9,111 +9,19 @@ export default async function page({
 }) {
   const page = searchParams["page"] ?? "1";
   const per_page = searchParams["per_page"] ?? "24";
+  const producer = searchParams["producer"] ?? "";
+  const flavor = searchParams["flavor"] ?? "";
 
   return (
     <div>
       <div className="min-h-screen flex justify-between pt-[130px]">
-        <div className="flex flex-col mx-7 justify-center items-center w-[400px] h-[70%] border-[1px] rounded-xl border-[#ccc]">
-          <div className="p-5 w-full flex justify-between items-center border-b-[1px] border-[#ccc]">
-            <span className=" text-2xl font-bold">Filters</span>
-            <button className="text-sm text-[#bbb] hover:text-[#aaa] select-none">
-              Clear all
-            </button>
-          </div>
-          <div className="w-full p-5">
-            <div className="flex flex-col mb-7">
-              <div className="font-bold mb-3">Producer</div>
-              <div className="w-full flex items-center gap-1.5">
-                <div className="my-1.5 border-[2px] w-5 h-5 border-[#aaa] p-[1px] rounded cursor-pointer select-none">
-                  {/* <CheckIcon /> */}
-                </div>
-                <div className="text-sm">
-                  Verde Mate <span className="text-[#999] ml-[2px]">(4)</span>
-                </div>
-              </div>
-              <div className="w-full flex items-center gap-1.5">
-                <div className="my-1.5 border-[2px] w-5 h-5 border-[#aaa] p-[1px] rounded cursor-pointer select-none">
-                  {/* <CheckIcon /> */}
-                </div>
-                <div className="text-sm">
-                  TasteVy <span className="text-[#999] ml-[2px]">(16)</span>
-                </div>
-              </div>
-            </div>
-            <div className="flex flex-col mb-7">
-              <div className="font-bold mb-3">Flavour</div>
-              <div className="w-full flex items-center gap-1.5">
-                <div className="my-1.5 border-[2px] w-5 h-5 border-[#aaa] p-[1px] rounded cursor-pointer select-none">
-                  {/* <CheckIcon /> */}
-                </div>
-                <div className="text-sm">
-                  Classic <span className="text-[#999] ml-[2px]">(19)</span>
-                </div>
-              </div>
-              <div className="w-full flex items-center gap-1.5">
-                <div className="my-1.5 border-[2px] w-5 h-5 border-[#aaa] p-[1px] rounded cursor-pointer select-none">
-                  {/* <CheckIcon /> */}
-                </div>
-                <div className="text-sm">
-                  Guarana <span className="text-[#999] ml-[2px]">(1)</span>
-                </div>
-              </div>
-            </div>
-            <div className="flex flex-col mb-7">
-              <div className="font-bold mb-3">Strength</div>
-              <div className="w-full flex items-center gap-1.5">
-                <div className="my-1.5 border-[2px] w-5 h-5 border-[#aaa] p-[1px] rounded cursor-pointer select-none">
-                  {/* <CheckIcon /> */}
-                </div>
-                <div className="text-sm">
-                  Very strong <span className="text-[#999] ml-[2px]">(19)</span>
-                </div>
-              </div>
-              <div className="w-full flex items-center gap-1.5">
-                <div className="my-1.5 border-[2px] w-5 h-5 border-[#aaa] p-[1px] rounded cursor-pointer select-none">
-                  {/* <CheckIcon /> */}
-                </div>
-                <div className="text-sm">
-                  Light <span className="text-[#999] ml-[2px]">(1)</span>
-                </div>
-              </div>
-            </div>
-            <div className="flex flex-col mb-7">
-              <div className="font-bold mb-3">Type of Leaves</div>
-              <div className="w-full flex items-center gap-1.5">
-                <div className="my-1.5 border-[2px] w-5 h-5 border-[#aaa] p-[1px] rounded cursor-pointer select-none">
-                  {/* <CheckIcon /> */}
-                </div>
-                <div className="text-sm">
-                  Green Mate <span className="text-[#999] ml-[2px]">(19)</span>
-                </div>
-              </div>
-              <div className="w-full flex items-center gap-1.5">
-                <div className="my-1.5 border-[2px] w-5 h-5 border-[#aaa] p-[1px] rounded cursor-pointer select-none">
-                  {/* <CheckIcon /> */}
-                </div>
-                <div className="text-sm">
-                  Green Mate with Fruits{" "}
-                  <span className="text-[#999] ml-[2px]">(1)</span>
-                </div>
-              </div>
-            </div>
-            <div className="flex flex-col">
-              <div className="font-bold mb-3">Origin</div>
-              <div className="w-full flex items-center gap-1.5">
-                <div className="my-1.5 border-[2px] w-5 h-5 border-[#aaa] p-[1px] rounded cursor-pointer select-none">
-                  {/* <CheckIcon /> */}
-                </div>
-                <div className="text-sm">
-                  Brazil <span className="text-[#999] ml-[2px]">(20)</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
         <Suspense fallback={<ProductSkeleton />}>
-          <Yerba page={page} per_page={per_page} />
+          <Yerba
+            page={page}
+            per_page={per_page}
+            producer={producer}
+            flavor={flavor}
+          />
         </Suspense>
       </div>
     </div>
--- app/ui/main/yerba/Yerba.tsx
+++ app/ui/main/yerba/Yerba.tsx
@@ -7,8 +7,9 @@ import { HeartIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Filter from "./Filter";
 
-export default async function Yerba({ page, per_page }: any) {
+export default async function Yerba({ page, per_page, producer, flavor }: any) {
   async function fetchData() {
     try {
       const res = await fetch(
@@ -25,26 +26,42 @@ export default async function Yerba({ page, per_page }: any) {
       return null;
     }
   }
+  const data = await fetchData();
+
+  let dataCount = data.length;
+
   const start = (Number(page) - 1) * Number(per_page);
   const end = start + Number(per_page);
-  const data = await fetchData();
-  const entries = data.slice(start, end);
-  const totalPages = Math.ceil(data.length / per_page);
+  let entries = data.slice(start, end);
+  let totalPages = Math.ceil(dataCount / per_page);
+
+  if (producer !== "") {
+    entries = data.filter((e: any) => e.producer === producer);
+    dataCount = entries.length;
+    totalPages = Math.ceil(dataCount / per_page);
+  }
+
+  if (flavor !== "") {
+    entries = data.filter((e: any) => e.flavor === flavor);
+    totalPages = Math.ceil(dataCount / per_page);
+  }
+
   return (
     <>
+      <Filter data={data} />
       <div className="w-[100%] mr-20 mb-10 flex flex-col items-center">
         <div className="w-[98%] px-2 py-4 border-y-[1px] border-[#ccc] mb-5 flex justify-between items-center">
-          <div className="text-[#888] text-sm">{data.length} results</div>
+          <div className="text-[#888] text-sm">{dataCount} results</div>
           <div className="flex gap-5">
             <DropSelect />
             <PaginationControls
-              hasNextPage={end < data.length}
+              hasNextPage={end < dataCount}
               hasPrevPage={start > 0}
               totalPages={totalPages}
             />
           </div>
         </div>
-        <div className="w-[100%] flex flex-wrap gap-x-[20px] gap-y-[40px] justify-center">
+        <div className="w-[100%] flex flex-wrap gap-x-[20px] gap-y-[40px] justify-start">
           {entries.map((e: any) => (
             <div
               key={e._id}
